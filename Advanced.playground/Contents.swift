758335576f4662414d2241e858078b9425709cef
Liu Yongjia <liuyongjia@ricebook.com>
2015-10-12 20:12:12 +0800

    first commit

---
+++ Advanced.playground/Contents.swift
@@ -0,0 +1,102 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let example = "Hello World"
+let DNS = "com.Serious.advanced"
+
+let dic = ["ZERO": 0, "One": 1]
+print(dic.description)
+
+
+let cache = NSCache()
+let name = "lastSep"
+
+cache.name = DNS + name
+print(cache.name)
+
+let path = NSSearchPathForDirectoriesInDomains(.CachesDirectory, NSSearchPathDomainMask.UserDomainMask, true)
+
+let view = UIStackView()
+
+
+let sbv1 = UIView()
+let sbv2 = UIView()
+view.addArrangedSubview(sbv1)
+view.addArrangedSubview(sbv2)
+
+view.subviews.forEach { (subView) -> () in
+    subView.removeFromSuperview()
+}
+
+extension UIColor {
+    
+    func colorWithHexString(stringToConvert: String, var alpha: Float) -> UIColor {
+        if alpha > 1.0 || alpha < 0.0 {
+            alpha = 1.0
+        }
+        
+        
+        
+        return UIColor()
+    }
+}
+
+// countElement 在 swift 2.0 里面已经 byebye 咯～
+var sentence = "for the first time and forever."
+print(sentence.lengthOfBytesUsingEncoding(NSUTF8StringEncoding))
+
+let array = ["A", "B"]
+for (i, j) in array.enumerate() {
+    print("\(i):\(j)")
+}
+
+var mini = min(8, 2, 3, 5)
+var maxi = max(8, 2, 3, 7)
+
+for i in ["B", "A"].sort() {
+    print(i)
+}
+
+var yichi = abs(-1)
+var yonnjuni = abs(-42)
+var yonnjuni2 = abs(42)
+
+print([26, 32, 12, 65, 55, 74].contains(32))
+
+
+var languages = ["swift", "Objective-C"]
+print(languages.joinWithSeparator("/"))
+
+print(languages.contains("swift"))
+print(languages.contains("Java"))
+
+var newLanguage = languages.dropFirst()
+newLanguage == ["Objective-C"]
+
+dump(languages)
+
+for i in (1...100).filter({$0 % 10 == 0}) {
+    print(i)
+    print(assert([10, 20, 30, 40, 50, 60, 70, 80, 90, 100].contains(i)))
+}
+
+"football".hasPrefix("foo")
+(10...100).startsWith(10...19)
+languages.startsWith(["swift"])
+
+[1, 2, 3].reverse() == [3, 2, 1]
+for i in ([1, 2, 3].reverse()) {
+    print(i)
+}
+
+print((1...10).minElement())
+print(languages.minElement())
+
+languages.reduce("", combine: {$0 + $1})
+[1, 2, 3].reduce(1, combine: {$0 * $1})
+
+
+
+
+
